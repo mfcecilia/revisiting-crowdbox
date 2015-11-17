e2651ba12b9410c3799860e3c13890d03c3a3f2d
Mykayla Fernandes <my_kaykay1@yahoo.com>
2015-11-16 22:09:19 -0500

    put functions inside the document ready function

--- js/crowdbox.js
+++ js/crowdbox.js
@@ -3,23 +3,50 @@ Scroll to the start position when
 the site loads and hide the excess content.
 **/
 $(document).ready(function(){
-  //  $('#signIn, #signUp, #crowdBox, #search').css('opacity', //'0.0');
+    $('#signIn, #signUp, #crowdBox, #search').fadeOut('slow');
+  //  $('#signIn, #signUp, #crowdBox, #search').css('opacity', '0.0');
     $('html, body').animate({
         scrollTop: $("#home").offset().top
-    }, 1000);
-    $('#signIn, #signUp, #crowdBox, #search').fadeOut('slow');
-});
+    }, 1000);  
+    console.log("done");
 
-$('#leftCenter').click(function(){    
-        $('#signIn').fadeIn('slow');
-});
 
-$('#rightCenter').click(function(){    
-        $('#signUp').fadeIn('slow');
-});
+    //Toggle sign in button and inputs
 
-/*$('#search').click(function(){    
-    $('#search').fadeOut('slow', function(){
-        $('#bottomCenter').fadeIn('slow');
+    $('#leftCenter').click(function(){    
+            $('#signIn').fadeIn('slow');
+        console.log("sign in");
     });
-});*/
+    
+    
+    //Toggle crowdbox button and inputs
+    $('#topCenter').click(function(){    
+            $('#crowdbox').fadeIn('slow');
+        console.log("crowdbox");
+    $('html, body').animate({
+        scrollIntoView: $("#crowdBox").offset().top
+    }, 1000);  
+    console.log("done");
+    });   
+
+
+    //Toggle sign up button and inputs
+    $('#rightCenter').click(function(){    
+            $('#signUp').fadeIn('slow');
+        console.log("sign up");
+    });
+
+
+    //Toggle search button and inputs
+    $('#bottomCenter').click(function(){    
+        $('#bottomCenter').fadeOut('slow', function(){
+            $('#search').fadeIn('slow');
+            console.log("search");
+        });
+
+
+
+    });
+
+
+});
